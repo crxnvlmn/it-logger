fc4ff83012aa7954b4c0c6f29c4555495bddccb7
Christian Villamin <ChristianVillamin31@gmail.com>
2019-09-25 15:58:35 +0800

    add tech action reducr

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -27,6 +27,52 @@ export const getTechs = () => async dispatch => {
   }
 };
 
+// Add technician to database
+export const addTech = tech => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs', {
+      method: 'POST',
+      body: JSON.stringify(tech),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
+// Delete tech from database
+export const deleteTech = () => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.statusText
+    });
+  }
+};
+
 // Set loading to true
 export const setLoading = () => {
   return {
--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addTech } from '../../actions/techActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddTechModal = () => {
+const AddTechModal = ({ addTech }) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
@@ -9,7 +12,12 @@ const AddTechModal = () => {
     if (firstName === '' || lastName === '') {
       M.toast({ html: 'Please enter a message and tech' });
     } else {
-      console.log(firstName, lastName);
+      addTech({
+        firstName,
+        lastName
+      });
+
+      M.toast({ html: `${firstName} ${lastName} is added as a technician.` });
 
       // Clear Fields
       setFirstName('');
@@ -63,4 +71,11 @@ const AddTechModal = () => {
   );
 };
 
-export default AddTechModal;
+AddTechModal.propTypes = {
+  addTech: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addTech }
+)(AddTechModal);
